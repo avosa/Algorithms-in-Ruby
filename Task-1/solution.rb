1b0a0c993c5fafaddd99fd6d529d9282ab4cf52b
Webster Avosa <websterb17@gmail.com>
2022-02-24 19:50:14 +0300

    Add some code explanation

--- Task-1/solution.rb
+++ Task-1/solution.rb
@@ -1,3 +1,18 @@
+=begin 
+Here's what the below class is doing:
+1. It first creates a hash of brackets and their closing counterparts.
+2. It then creates an empty stack and a counter variable.
+3. It then iterates through the input string.
+4. If the character is an opening bracket, it pushes it to the stack.
+5. If the character is a closing bracket, it checks if the stack is empty.
+6. If it is, it raises an exception.
+7. If it isn't, it checks if the closing bracket matches the opening bracket at the top of the stack.
+8. If it doesn't, it raises an exception.
+9. If it does, it pops the opening bracket from the stack and increments the counter.
+10. If the stack is empty at the end of the iteration, it returns the counter.
+11. If it isn't, it raises an exception.
+=end
+
 class BracketsCounter
     def initialize
       @brackets = {'(' => ')', '{' => '}', '[' => ']'}
@@ -29,3 +44,5 @@ class BracketsCounter
   input = "begin(wo(dog)od)middle(ste(ca(bridge)t)el)end., prints '5' 
   counter = BracketsCounter.new
   puts counter.count_brackets(input)
+
+
