54970f8b0426daf878afd678974be74d579b4785
Webster Avosa <websterb17@gmail.com>
2022-02-25 09:41:55 +0300

    Add Task 2

---
+++ Task-2/solution.rb
@@ -0,0 +1,19 @@
+=begin
+Here's what the below method is doing:
+1. It iterate through each character in the string input
+2. It checks if the current character is equal to the next character
+3. If it is, it returns the current character
+4. If it isn't, it continues to the next character
+5. If it reach the end of the string, it return nil
+=end
+def first_repeated_character(string)
+    string.chars.each do |char|
+      return char if string.count(char) > 1
+    end
+    nil
+  end
+  
+  puts first_repeated_character("ABCA")
+  puts first_repeated_character("BCABA")
+  puts first_repeated_character("glovol")
+  puts first_repeated_character("ABC")
